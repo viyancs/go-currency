b0fbbc4a6a88b7272500bd371b8a93f1576158b5
viyancs <msofyancs@gmail.com>
2018-07-26 17:57:50 +0700

    update by improvment path and cmd task

--- models/schema.go
+++ models/schema.go
@@ -5,7 +5,7 @@
  *  make sure always create simple code and clean code,
  *  always give mark comment for easy maintenancen
  */
-package main
+package models
 import (
     "github.com/astaxie/beego/orm"
     "time"
@@ -18,9 +18,10 @@ type Dailyexchange struct {
     Id   int    `orm:"column(id)"`
     C_from string   `orm:"size(3)"`
     C_to string   `orm:"size(3)"`
-    Rate float64
-    Date time.Time
+    Rate float64    `orm:"null"`
+    Date time.Time  `orm:"null"`
 }
+
 
 func init(){
     orm.RegisterModel(new(Dailyexchange))
@@ -28,7 +29,47 @@ func init(){
     orm.RegisterDataBase("default", "mysql", "root:@/test_go?charset=utf8", 30)
 }
 
-func main() {
+func GetDailyexchange(params ...string) []orm.Params{
+    o := orm.NewOrm()
+    var maps []orm.Params
+
+    date := "";
+    if len(params) > 0 {
+        date = params[0] //set date srting in 0 index
+    }
+
+    if (len(date) > 0) {
+        query := "select id,c_from,c_to,date,IF(rate = '0', 'insufficient data', rate) as rate,IFNULL(ROUND(AVG(CASE WHEN date between (date - INTERVAL 2 DAY) and date then rate else 0 end), 2),'insufficient data') AS 'last 2 days' from dailyexchange where date = ? group by c_from,c_to"
+        o.Raw(query,date).Values(&maps)
+        return maps
+    }
+
+    o.Raw("select * from dailyexchange").Values(&maps)
+    return maps
+
+}
+
+func SaveDailyexchange(d Dailyexchange) int64{
+    o := orm.NewOrm()
+    r,err := o.Insert(&d)
+    if err == nil {
+        return r
+    }
+    return 0
+}
+
+func DelDailyexchange(from string, to string) int64{
+    o := orm.NewOrm()
+    _, err := o.Raw("delete FROM dailyexchange where c_from = ? and c_to = ?").SetArgs(from, to).Exec()
+    if err == nil {
+        return 1
+    }
+    return 0
+
+}
+
+//this cmd for cli comand
+func Cmd() {
     if (len(os.Args) < 2) {
         content := `schema command usage:
 
@@ -59,6 +100,8 @@ func sample() {
         {C_from: "GBP",C_to:"INR",Rate:90.4866,Date:twofive},
         {C_from: "USD",C_to:"IDR",Rate:14750,Date:twofive},
         {C_from: "USD",C_to:"GBP",Rate:0.761913,Date:twofive},
+        {C_from: "USD",C_to:"JPN",Date:twofive},
+        {C_from: "JPN",C_to:"GBP",Date:twofive},
     }
     successNums, err := o.InsertMulti(100, de)
     if err == nil {
