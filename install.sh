2115fbdea36609b09d79297a0fa1584fb31b2fdf
viyancs <msofyancs@gmail.com>
2018-07-28 14:25:08 +0700

    adding install exectable file

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#creating at 28/07/2018
+#created by viyancs
+mkdir go-currency
+cd go-currency
+git clone https://github.com/viyancs/go-currency.git .
+cd src
+docker build -t viyancs/golang-mysql-api --no-cache=true .
+docker run --name mysqldb -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test_go -e  MYSQL_ROOT_HOST=% -d mysql:5.7
+docker run -p 8282:8282 --name currency-go -d viyancs/golang-mysql-api
+docker exec -ti currency-go sh -c "cd cmd/schema && go build -a -o bintask task.go && ./bintask orm syncdb -force && ./bintask sample"
