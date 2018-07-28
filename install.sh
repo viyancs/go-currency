b8a64801771e09eeaf9f8021cf37dcfdcd090aa3
viyancs <msofyancs@gmail.com>
2018-07-28 15:00:36 +0700

    adding link container mysqldb

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ git clone https://github.com/viyancs/go-currency.git .
 cd src
 docker build -t viyancs/golang-mysql-api --no-cache=true .
 docker run -p 3306:3306 --name mysqldb -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=test_go -e MYSQL_ROOT_HOST=% -d mysql:5.7
-docker run -p 8282:8282 --name currency-go -d viyancs/golang-mysql-api
+docker run -p 8282:8282 --link mysqldb:mysqldb --name currency-go -d viyancs/golang-mysql-api
 docker exec -ti currency-go sh -c "cd cmd/schema && go build -a -o bintask task.go && ./bintask orm syncdb -force && ./bintask sample"
